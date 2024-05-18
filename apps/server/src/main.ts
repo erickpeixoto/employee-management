82ae2c2a06a85d8840f06710d70d9cb90fd7f971
erickpeixoto <erickepeixoto@gmail.com>
2024-05-18 15:56:31 -0300

    doc: add swagger to manage our documentation process

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,24 @@
+// src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack Assessment')
+    .setDescription('The Fullstack Assessment API description')
+    .setVersion('1.0')
+    .addTag('employees')
+    .addTag('departments')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
