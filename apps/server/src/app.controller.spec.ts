b199a45dc3b7b98007920b83c217b1ebf33d276c
erickpeixoto <erickepeixoto@gmail.com>
2024-05-19 17:37:03 -0300

    test: main app controller testing process

---
+++ apps/server/src/app.controller.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  const mockAppService = {
+    getHello: jest.fn().mockReturnValue('Hello World!'),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockAppService,
+        },
+      ],
+    }).compile();
+
+    appController = module.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
+  });
+
+  it('should return "Hello World!"', () => {
+    expect(appController.getHello()).toBe('Hello World!');
+    expect(appService.getHello).toHaveBeenCalled();
+  });
+});
