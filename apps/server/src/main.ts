d8282d85269789973b7139e4b4689de4a6668bed
erickpeixoto <erickepeixoto@gmail.com>
2024-05-19 17:38:30 -0300

    test: prepare app main to be tested

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,21 +1,22 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { generateOpenApi } from '@ts-rest/open-api';
+import { contract } from 'ts-contract';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Fullstack Assessment')
-    .setDescription('The Fullstack Assessment API description')
-    .setVersion('1.0')
-    .addTag('employees')
-    .addTag('departments')
+  const options = new DocumentBuilder()
+    .setTitle('Fullstack Assessment API')
+    .setDescription(
+      'API documentation for the Fullstack Assessment application',
+    )
+    .setVersion('1.0.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = generateOpenApi(contract, options);
+
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
