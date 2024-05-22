bc42d25eac414b45c36d3f21fc64d18596f69577
erickpeixoto <erickepeixoto@gmail.com>
2024-05-22 07:53:08 -0300

    feat(frontend): add Navbar component

---
+++ apps/client/src/components/navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+  return (
+    <div className="flex justify-between items-center p-5 border-b-slate-200 border">
+      <div className="font-bold md:text-2xl">Employee Management</div>
+      <div className="
+        flex items-center
+        space-x-4
+        text-sm
+        text-slate-500
+        font-semibold
+      ">Hello, User</div>
+    </div>
+  );
+}
