5a725f9f5582e741adf89f0fa503f8b5881a9f18
erickpeixoto <erickepeixoto@gmail.com>
2024-05-22 13:38:58 -0300

    feat(frontend): add ErrorUI component

---
+++ apps/client/src/components/error-ui.tsx
@@ -0,0 +1,8 @@
+export function ErrorStatus() {
+  return (
+    <div className="bg-red-100 border text-red-500 border-red-400 text-red-700px-4 py-3 mt-4 mb-4 place-items-center grid">
+      <strong className="font-bold">Error!</strong>
+      <span className="block sm:inline">Something went wrong.</span>
+    </div>
+  );
+}
