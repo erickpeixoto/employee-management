5cdade98a4679780bda2f5959f110fd4f97f6f25
erickpeixoto <erickepeixoto@gmail.com>
2024-05-24 19:18:57 -0300

    feat(frontend): adjust department form

--- apps/client/src/components/employee/department/form.tsx
+++ apps/client/src/components/employee/department/form.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { apiClientQuery } from "ts-contract";
-import { Button } from "@/components/ui/button";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { useState, useEffect } from "react";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
 import { useForm, Controller } from "react-hook-form";
 import { Employee } from "ts-contract";
-import { useQueryClient } from '@ts-rest/react-query/tanstack';
-import { useEffect, useState } from "react";
-
-
+import { useQueryClient } from "@tanstack/react-query";
+import { apiClientQuery } from "ts-contract";
+import { Button } from "@/components/ui/button";
 
 interface UpdateDepartmentFormProps {
   employee: Employee;
@@ -16,9 +20,11 @@ interface UpdateDepartmentFormProps {
 
 export function DepartmentForm({ employee }: UpdateDepartmentFormProps) {
   const [isChanged, setIsChanged] = useState(false);
-  const { data: departmentsData, isLoading: departmentsLoading } = apiClientQuery.departments.getAll.useQuery(["departments"]);
   const queryClient = useQueryClient();
-  const { control, handleSubmit, watch } = useForm<Employee>({
+  const { data: departmentsData, isLoading: departmentsLoading } =
+    apiClientQuery.departments.getAll.useQuery(["departments"]);
+
+  const { control, handleSubmit, watch, setValue } = useForm<Employee>({
     defaultValues: {
       departmentId: employee.departmentId || 1,
     },
@@ -26,9 +32,15 @@ export function DepartmentForm({ employee }: UpdateDepartmentFormProps) {
 
   useEffect(() => {
     const subscription = watch((value, { name }) => {
-      if (name === "departmentId" && value.departmentId !== employee.departmentId) {
+      if (
+        name === "departmentId" &&
+        value.departmentId !== employee.departmentId
+      ) {
         setIsChanged(true);
-      } else {
+      } else if (
+        name === "departmentId" &&
+        value.departmentId === employee.departmentId
+      ) {
         setIsChanged(false);
       }
     });
@@ -38,11 +50,11 @@ export function DepartmentForm({ employee }: UpdateDepartmentFormProps) {
   const { mutate } = apiClientQuery.employees.update.useMutation({
     onSuccess: () => {
       queryClient.invalidateQueries(["employees", String(employee.id)]);
+      setIsChanged(false);
     },
     onError: (error) => {
       console.error(error);
     },
-    
   });
 
   if (departmentsLoading) {
@@ -61,21 +73,25 @@ export function DepartmentForm({ employee }: UpdateDepartmentFormProps) {
         address: employee.address,
         avatar: employee.avatar,
         hireDate: employee.hireDate,
-      }
+      },
     });
-  }
+    setValue("departmentId", data.departmentId);
+  };
 
   return (
     <form onSubmit={handleSubmit(processSubmit)} className="mt-6">
       <h3 className="text-xl font-semibold">Update Department</h3>
       <div className="flex items-center gap-4 mt-2">
-      <Controller
+        <Controller
           name="departmentId"
           control={control}
           render={({ field }) => (
             <Select
               value={field.value ? String(field.value) : ""}
-              onValueChange={field.onChange}
+              onValueChange={(value) => {
+                field.onChange(value);
+                setIsChanged(value !== String(employee.departmentId));
+              }}
             >
               <SelectTrigger className="w-[240px]">
                 <SelectValue placeholder="Select a department" />
@@ -90,7 +106,12 @@ export function DepartmentForm({ employee }: UpdateDepartmentFormProps) {
             </Select>
           )}
         />
-         <Button color={isChanged ? "success" : "outline"} type="submit" disabled={!isChanged}>
+        <Button
+          color={isChanged ? "success" : "outline"}
+          variant={isChanged ? "secondary" : "outline"}
+          type="submit"
+          disabled={!isChanged}
+        >
           Update
         </Button>
       </div>
