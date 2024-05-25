29f3a938c95cecd08b01c5e28eff2618fd4be1bc
erickpeixoto <erickepeixoto@gmail.com>
2024-05-25 18:15:30 -0300

    chore(test): remove console.log

--- apps/server/test/testSequencer.js
+++ apps/server/test/testSequencer.js
@@ -2,7 +2,6 @@ const Sequencer = require('@jest/test-sequencer').default;
 
 class CustomSequencer extends Sequencer {
   sort(tests) {
-    console.log('Sorting tests');
     return tests.sort((a, b) => (a.path > b.path ? 1 : -1));
   }
 }
