e69a427c1434271253c0408bdf88db6865e45046
erickpeixoto <erickepeixoto@gmail.com>
2024-05-30 16:40:28 -0300

    ci(frontend): adjust process

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { ListEmployee } from "@/components/employee/list";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <main>
-      <ListEmployee title="All People" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ListEmployee title="All People" />
+      </Suspense>
     </main>
   );
 }
